7773ebb22a9f880cc73cbc4a78c51587fa466511
Oh-Tae-Yang <tyo1012@naver.com>
2025-03-23 09:27:40 +0000

    COMMITTED BY Oh-Tae-Yang: Processed 12911. 다음 큰 숫자

--- Programmers/Oh-Tae-Yang/12911. 다음 큰 숫자/다음 큰 숫자.java
+++ Programmers/Oh-Tae-Yang/12911. 다음 큰 숫자/다음 큰 숫자.java
@@ -30,3 +30,17 @@ class Solution {
         return answer;
     }
 }
+
+/**
+ * 1. n의 다음 큰 숫자는 n보다 큰 자연수 입니다.
+ * 2. n의 다음 큰 숫자는 n의 2진수로 변환했을 때 1의 갯수가 같습니다.
+ * 3. n의 다음 큰 숫자는 조건 1,2를 만족하는 수 중 가장 작은 수 입니다.
+ *
+ * -------------------------------------------------------------
+ * solution 1
+ * java.lang.Integer의  toBinaryString(), toOctalString(), toHexaString() 메소드를 이용하여
+ * 10진수를 2진수, 8진수, 16진수 문자열로 변환할 수 있습니다.
+ *
+ * solution 2
+ * Integer.bitCount: 정수를 매개변수로 넣었을때 그 정수의 1 의 개수를 반환한다.
+ */
